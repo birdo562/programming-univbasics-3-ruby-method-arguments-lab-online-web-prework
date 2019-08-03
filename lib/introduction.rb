4481ac3dd5095adc5fef8046323287f2502c9841
attentive-rubyist-8517 <robby.medina562@gmail.com>
2019-08-03 05:13:25 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,7 +8,7 @@ puts "Hi, my name is #{name} and i am learning to program in #{language}."
 end
 introduction_with_language("Dan", "Ember")
 
-def introduction_with_language_optional(name, language="ruby")
+def introduction_with_language_optional(name, language="Ruby")
 puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
